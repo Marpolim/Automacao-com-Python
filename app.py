0f3216ab17f923c29c1fb3a7480ddefacd3f6dec
Joao Vitor Leite Magalhaes <jvmagalhaes29@hotmail.com>
2023-03-06 00:12:27 -0400

    Automação para iniciar partida LOL

---
+++ app.py
@@ -0,0 +1,8 @@
+import pyautogui
+from time import sleep
+
+pyautogui.doubleClick(45,299,duration=2)
+pyautogui.click(965,858,duration=20)
+pyautogui.click(439,223,duration=10)
+pyautogui.click(863,865,duration=4)
+pyautogui.click(843,863,duration=6)
